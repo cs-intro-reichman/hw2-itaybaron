c969745c8adc4995d60f17542aaa8dd68efa6db2
itay BARON <itaybaron@Mac.lan>
2025-11-08 13:14:26 +0200

    Completed assignment

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,28 @@
-//feedback
 
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
-        }
+	
+                String Cheer = args[0].toUpperCase();;
+                int N = Integer.parseInt(args[1]);
+                int i = 0;
+
+                String anLetters = "AEFHILMNORSX";
+
+                while (i < Cheer.length()) {
+                    char letter = Cheer.charAt(i);
+
+                    boolean useAn = anLetters.indexOf(letter) >= 0;
+                    String article = useAn ? "an" : "a ";
+
+                    System.out.println("Give me " + article + " " + letter + ": " + letter + "!");
+                    i++;
+                }
+
+                System.out.println("What does that spell?");
+                int count = 0;
+                while(count < N){
+                        System.out.println(Cheer + "!!!");
+                        count++;
+                }
+ }
 }
