b4eaf9df70e52ae0de3582d7a27055efa7c9501c
itay BARON <itaybaron@Mac.lan>
2025-11-08 13:28:15 +0200

    Completed assignment

--- Collatz.java
+++ Collatz.java
@@ -3,37 +3,40 @@ public class Collatz {
 	public static void main(String args[]) {
 	
         int N = Integer.parseInt(args[0]);
-        String mode = args[1];       
+        String mode = args[1];
 
         for (int seed = 1; seed <= N; seed++) {
             int current = seed;
             int steps = 1;
 
-			
             if (mode.equals("v")) {
-                System.out.print(current + " ");
-            }
 
-
-            while (current != 1) {
-                if (current % 2 == 0) {
-                    current = current / 2;
-                } else {
-                    current = 3 * current + 1;
-                }
-                steps++;
-
-                if (mode.equals("v")) {
+                while (true) {
                     System.out.print(current + " ");
+                    if (current == 1 && steps > 1) {
+                        break;
+                    }
+                    if (current % 2 == 0) {
+                        current = current / 2;
+                    } else {
+                        current = 3 * current + 1;
+                    }
+                    steps++;
                 }
-            }
-
-            if (mode.equals("v")) {
                 System.out.println("(" + steps + ")");
+            } else {
+                while (!(current == 1 && steps > 1)) {
+                    if (current % 2 == 0) {
+                        current = current / 2;
+                    } else {
+                        current = 3 * current + 1;
+                    }
+                    steps++;
+                }
             }
         }
 
-        
         System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
-	}
+    }
 }
+
